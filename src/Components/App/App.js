78a62bfc6f8ce541c9c354bbdb58734f59aabd81
Luke Lechel <13129882+lukelechel@users.noreply.github.com>
2020-09-29 19:58:18 -0400

    Added React Router

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,12 +1,21 @@
 import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import './App.css'
 
+import Home from '../Home/Home'
+import Error from '../Error/Error'
+
 function App() {
-  return (
-    <div>
-      <h1>hi</h1>
-    </div>
-  )
+    return (
+        <BrowserRouter>
+            <div>
+                <Switch>
+                    <Route path="/" component={Home} exact />
+                    <Route component={Error} />
+                </Switch>
+            </div>
+        </BrowserRouter>
+    )
 }
 
 export default App
