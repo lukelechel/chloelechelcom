2739b64fcd42a4ec936657ae6822b69379adb17d
Luke Lechel <13129882+lukelechel@users.noreply.github.com>
2020-09-29 20:03:50 -0400

    Resolved React default config errors

---
+++ src/Components/Error/Error.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Error.css'
+
+function Error() {
+    return (
+        <div>
+            <h1>error</h1>
+            <h1><a href="/" alt="home">go home</a></h1>
+        </div>
+    )
+}
+
+export default Error
