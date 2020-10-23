3aa81b95f452ad5439dae0145bacbc0394b511d1
Luke Lechel <13129882+lukelechel@users.noreply.github.com>
2020-10-23 14:49:10 -0400

    Added Weddings portfolio genre

--- src/Components/PortfolioGenres/PortfolioGenres.js
+++ src/Components/PortfolioGenres/PortfolioGenres.js
@@ -5,21 +5,26 @@ import photoData from '../../PhotoData.json'
 const awsResourceLibWorkPrefix = "https://chloelechelcom-resources.s3.us-east-2.amazonaws.com/work-samples/"
 
 const portfolioGenreLibrary = [
+
     {
-        categoryTitle: "portraits",
-        fileName: "CAB8171E-3485-4AAB-9875-AD86EF8957CF"
-    },
-    {
-        categoryTitle: "couples",
-        fileName: "25D111F2-7266-4792-B73E-365BB171438B"
+        categoryTitle: "weddings",
+        fileName: "IMG_4269"
     },
     {
         categoryTitle: "nature",
         fileName: "5740DA5D-ADEA-44A5-BFD3-459B207CB066"
     },
     {
+        categoryTitle: "couples",
+        fileName: "25D111F2-7266-4792-B73E-365BB171438B"
+    },
+    {
         categoryTitle: "travel",
         fileName: "F24CBA41-B716-4A13-B702-85049064DEF1"
+    },
+    {
+        categoryTitle: "portraits",
+        fileName: "CAB8171E-3485-4AAB-9875-AD86EF8957CF"
     }
 ]
 
