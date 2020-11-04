83e6e27e76abad6aa7c420943d6a25782bb7a040
Luke Lechel <13129882+lukelechel@users.noreply.github.com>
2020-11-04 14:52:24 -0500

    Updated Error page, cleaned up

--- src/Components/Error/Error.js
+++ src/Components/Error/Error.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import './Error.css'
 
+import NavBar from '../NavBar/NavBar'
+
 function Error() {
     return (
-        <div>
-            <h1>error</h1>
-            <h1><a href="/" alt="home">go home</a></h1>
+        <div id="error-container">
+            <NavBar />
+            <div className="pageContent">
+                <h1>404</h1>
+                <p>Sorry, what you're looking for isn't here. Let's get you going in the right direction again.</p>
+                <h1><a href="/" alt="home">Go Home</a></h1>
+            </div>
         </div>
     )
 }
